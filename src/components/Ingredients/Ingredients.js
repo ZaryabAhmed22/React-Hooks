488b40f70712cd639dc74559a0bb2202d5b448ae
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-03-16 21:50:53 +0500

    Lifting the state up

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients, //-- taking all the previos ingredients by using the spread operator.
+      { id: Math.random().toString(), ...ingredient }, //adding the new ingredient and its id and then using the spread operator to take out the key value pair out of the array.
+    ]);
+  };
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
